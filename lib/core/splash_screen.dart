30366b7b19acf7ea807ca4482fae2d0f02b63e70
samitkapoor <samitkapoor77@gmail.com>
2022-02-01 23:42:06 +0530

    tweaked animations

--- lib/core/splash_screen.dart
+++ lib/core/splash_screen.dart
@@ -24,12 +24,12 @@ class _SplashScreenState extends State<SplashScreen>
       vsync: this,
       duration: const Duration(seconds: 3),
     );
-    animation = Tween<double>(begin: Get.height, end: -200).animate(
+    animation = Tween<double>(begin: 800, end: -200).animate(
       CurvedAnimation(
           parent: animationController!, curve: Curves.fastOutSlowIn),
     );
 
-    animationController?.forward();
+    // animationController?.forward();
     super.initState();
   }
 
@@ -41,7 +41,11 @@ class _SplashScreenState extends State<SplashScreen>
 
   @override
   Widget build(BuildContext context) {
-    Future.delayed(const Duration(milliseconds: 1200)).then((value) {
+    Future.delayed(const Duration(milliseconds: 500)).then((value) {
+      animationController?.forward();
+    });
+
+    Future.delayed(const Duration(milliseconds: 1500)).then((value) {
       Navigator.of(context).pushReplacement(
         CustomPageRoute(child: ProfileScreen()),
       );
@@ -63,7 +67,7 @@ class _SplashScreenState extends State<SplashScreen>
           AnimatedBuilder(
             animation: animationController!,
             builder: (ctx, child) {
-              return Positioned(top: animation!.value, child: child as Widget);
+              return Positioned(top: animation!.value, child: child!);
             },
             child: SizedBox(
               width: Get.width,
